20c58a60f66934507833ca727df8efeda9467656
Hugh Cunningham <hugh.e.cunningham@gmail.com>
2020-08-15 13:29:51 -0700

    Implement shape plotting
    - Use xkcd for hand-drawn plot appearance
    - Annotate axes to emulate Vonnegut plots

--- story_shapes/story_shapes.py
+++ story_shapes/story_shapes.py
@@ -4,6 +4,7 @@ from copy import deepcopy
 from typing import Iterator
 from typing import List
 
+from matplotlib import pyplot as plt
 from transformers import pipeline
 
 START_OF_STORY = "*** START OF THIS PROJECT GUTENBERG EBOOK"
@@ -17,15 +18,15 @@ def main(story_path: str) -> None:
     segments = iter_segments(story_path, tokenizer, 50)
 
     sentiments = []
-    for window in iter_windows(segments, 8):
+    for window in iter_windows(segments, 5):
         window_text = "\n".join(window)
-        window_sentiment = sentiment_analyzer(window_text)
+        window_sentiment = sentiment_analyzer(window_text)[0]
         if window_sentiment["label"] == "POSITIVE":
             sentiments.append(window_sentiment["score"])
         else:
             sentiments.append(-window_sentiment["score"])
 
-    plot_sentiments(sentiments)
+    plot_shape(sentiments)
 
 
 def iter_windows(segments: Iterator[str], window_length: int) -> Iterator[List[str]]:
@@ -78,8 +79,60 @@ def get_token_count(tokenizer, text: str) -> int:
     return len(tokenized_text["input_ids"]) - 2
 
 
-def plot_sentiments(sentiments: List[float]) -> None:
-    pass
+def plot_shape(sentiments: List[float], title: str, shape_path: str) -> None:
+    with plt.xkcd():
+        ax = plt.gca()
+        ax.plot(sentiments, color="black")
+        ax.set_xlim(-0.5, len(sentiments))
+        ax.set_ylim(-1, 1)
+        ax.axis("off")
+        ax.axhline(0, color="dimgray")
+        ax.axvline(0, color="dimgray")
+        ax.annotate(
+            "BEGINNING",
+            xy=(-0.25, 0.5),
+            ha="left",
+            va="center",
+            fontsize=12,
+            xycoords="axes fraction",
+            color="dimgray",
+        )
+        ax.annotate(
+            "END",
+            xy=(1.1, 0.5),
+            ha="right",
+            va="center",
+            fontsize=12,
+            xycoords="axes fraction",
+            color="dimgray",
+        )
+        ax.annotate(
+            "GOOD FORTUNE",
+            xy=(0, 1.05),
+            ha="center",
+            va="center",
+            fontsize=12,
+            xycoords="axes fraction",
+            color="dimgray",
+        )
+        ax.annotate(
+            "ILL FORTUNE",
+            xy=(0, -0.05),
+            ha="center",
+            va="center",
+            fontsize=12,
+            xycoords="axes fraction",
+            color="dimgray",
+        )
+        ax.annotate(
+            title.upper(),
+            xy=(0.75, -0.1),
+            va="bottom",
+            fontsize=16,
+            xycoords="axes fraction",
+        )
+        plt.tight_layout()
+        plt.savefig(shape_path)
 
 
 if __name__ == "__main__":
